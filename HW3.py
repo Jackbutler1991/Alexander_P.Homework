1dabc8a0ebaf68cec8463e2d1325de84aa5dc653
Jackbutler1991 <djackbutler1991@gmail.com>
2023-02-06 22:26:01 +0300

    add pr3

---
+++ HW3.py
@@ -0,0 +1,16 @@
+#Задание 1.2
+a = 1
+a1 = 1.1
+b = 'asd'
+lst = [1, 2, 3, 4]
+d = {'dasfd': 1, 'ghjk': 2}
+bo = True
+set_1 = (1, 2, 3)
+
+print(type(a))
+print(type(a1))
+print(type(b))
+print(type(lst))
+print(type(d))
+print(type(bool))
+print(type(set_1))
